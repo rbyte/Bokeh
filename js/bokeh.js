a5f4de20a4aa3332ca21d04b546602c4a59f4af3
matmax <matthias.graf@st.ovgu.de>
2013-12-10 16:40:08 +0100

    script function frame

--- js/bokeh.js
+++ js/bokeh.js
@@ -1,7 +1,22 @@
 
-function interfaceInit() {
-	d3.select("#feGaussianBlur3765")
-		.transition()
-		.duration(5000)
-		.attr("stdDeviation", 50)
+bokeh = function() { // spans everything - not indented
+var bokeh = {}
+
+bokeh.init = function () {
+//	d3.select("#feGaussianBlur3765")
+//		.transition()
+//		.duration(5000)
+//		.attr("stdDeviation", 50)
+//	bla()
 }
+
+function bla() {
+	console.log("bla")
+}
+
+
+
+
+return bokeh
+}()
+
