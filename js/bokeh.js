c64ce77b015b0497c1a09462ffabb2e85b17bac2
matmax <matthias.graf@st.ovgu.de>
2014-02-01 17:45:41 +0100

    added code for transitions along a path

--- js/bokeh.js
+++ js/bokeh.js
@@ -2,6 +2,7 @@
 bokeh = function() { // spans everything - not indented
 var bokeh = {}
 
+
 bokeh.init = function () {
 	var svg = d3.select("#svg")
 //	var c = circle(svg, 350, 350, 1)
@@ -22,6 +23,64 @@ bokeh.init = function () {
 		.attr("transform", "translate("+500+", "+700+") scale("+2+")")
 	
 	saveSVGshortcut()
+	
+	// http://bl.ocks.org/mbostock/1705868
+	
+var points = [
+[480, 200],
+[580, 400],
+[680, 100],
+[780, 300],
+[180, 300],
+[280, 100],
+[380, 400]
+]
+
+var path = svg.append("path")
+	.data([points])
+	.attr("d", d3.svg.line()
+		.tension(0) // Catmull–Rom
+		.interpolate("cardinal-closed"))
+
+var circle = svg.append("circle")
+	.attr("r", 13)
+	.attr("transform", "translate(" + points[0] + ")")
+
+transition()
+
+function transition() {
+	circle.transition()
+		.duration(10000)
+		.attrTween("transform", translateAlong(path.node()))
+		.each("end", transition)
+}
+
+// Returns an attrTween for translating along the specified path element.
+function translateAlong(path) {
+	var l = path.getTotalLength()
+	return function(d, i, a) {
+		return function(t) {
+			var p = path.getPointAtLength(t * l)
+			return "translate(" + p.x + "," + p.y + ")"
+		}
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 function circlePath(a) {
