e3ab29e0300a706dff4a53572d7584ff07051330
matmax <matthias.graf@st.ovgu.de>
2014-02-07 16:23:20 +0100

    refactoring and stepping gaussian too

--- js/bokeh.js
+++ js/bokeh.js
@@ -6,16 +6,10 @@ stopItNow = false
 
 bokeh.init = function () {
 	var svg = d3.select("#svg")
-	
-	var c = new Particle(svg, 350, 350, 60, 140, 133, 197, 0.6, 5)
-	var c2 = new Particle(svg, 300, 300, 60, 140, 133, 197, 0.6, 5)
+	var c = new Particle(350, 350, 60, 140, 133, 197, 0.6, 5)
+	var c2 = new Particle(300, 300, 60, 140, 133, 197, 0.6, 5)
 	
 	var psys = new PSystem()
-	psys.addParticle(new Particle(svg, 650, 650, 60, 50, 133, 197, 0.6, 5))
-	psys.addParticle(new Particle(svg, 500, 650, 60, 100, 133, 197, 0.6, 5))
-	psys.addParticle(new Particle(svg, 650, 500, 60, 150, 133, 197, 0.6, 5))
-	psys.addParticle(new Particle(svg, 500, 500, 60, 180, 133, 197, 0.6, 5))
-	psys.start()
 	
 //	if (false)
 	d3.select("#feGaussianBlur3765")
@@ -33,56 +27,252 @@ bokeh.init = function () {
 	saveSVGshortcut()
 	
 	// http://bl.ocks.org/mbostock/1705868
-	
-var points = [
-[480, 200],
-[580, 400],
-[680, 100],
-[780, 300],
-[180, 300],
-[280, 100],
-[380, 400]
-]
+	var points = [
+	[480, 200],[580, 400],[680, 100],[780, 300],[180, 300],[280, 100],[380, 400]
+	]
 
-var path = svg.append("path")
-	.data([points])
-	.attr("d", d3.svg.line()
-		.tension(0) // Catmull–Rom
-		.interpolate("cardinal-closed"))
-//	.style({
-//		"fill": "none",
-//		"stroke": hsl255ToHex(140, 133, 255),
-//		"stroke-opacity": 1,
-//		"stroke-width": "3px"
-//	})
-if (false)
-transition()
+	var path = svg.append("path")
+		.data([points])
+		.attr("d", d3.svg.line()
+			.tension(0) // Catmull–Rom
+			.interpolate("cardinal-closed"))
+	//	.style({
+	//		"fill": "none",
+	//		"stroke": hsl255ToHex(140, 133, 255),
+	//		"stroke-opacity": 1,
+	//		"stroke-width": "3px"
+	//	})
+	if (false)
+	transition()
 
-function transition() {
-	c2.obj.transition()
-		.duration(10000)
-		.ease(d3.ease("linear"))
-//		.style("fill", "#ff0000")
-		.styleTween("fill", rainbow)
-		.attrTween("transform", translateAlong(path.node()))
-		.each("end", transition)
-}
+	function transition() {
+		c2.obj.transition()
+			.duration(10000)
+			.ease(d3.ease("linear"))
+	//		.style("fill", "#ff0000")
+			.styleTween("fill", rainbow)
+			.attrTween("transform", translateAlong(path.node()))
+			.each("end", transition)
+	}
 
-// Returns an attrTween for translating along the specified path element.
-function translateAlong(path) {
-	var l = path.getTotalLength()
-	return function(d, i, a) {
-		return function(t) { // is an interpolator, t is in [0,1]
-			var p = path.getPointAtLength(t * l)
-			return "translate(" + p.x + "," + p.y + ")"
+	// Returns an attrTween for translating along the specified path element.
+	function translateAlong(path) {
+		var l = path.getTotalLength()
+		return function(d, i, a) {
+			return function(t) { // is an interpolator, t is in [0,1]
+				var p = path.getPointAtLength(t * l)
+				return "translate(" + p.x + "," + p.y + ")"
+			}
 		}
 	}
-}
 
-function rainbow(d, i, a) {
-	return function(t) {
-		return hsl255ToHex(t*255, 255, 125)
+	function rainbow(d, i, a) {
+		return function(t) {
+			return hsl255ToHex(t*255, 255, 125)
+		}
 	}
+
+}
+
+
+
+
+
+
+
+
+
+
+function Particle(x,y,r,h,l,s,a,g) {
+	var self = this
+	
+	function Prop(initial) {
+//		this.parent = self
+		var self = this
+		self._ = initial // value
+		
+		self.v = 0 // velocity
+		self.acc = 0 // acceleration
+		self.activity = 0
+		self.activityRounds = 0
+	}
+	
+	self.x = new Prop(x)
+	self.y = new Prop(y)
+	
+	self.h = new Prop(h) // hue
+	self.s = new Prop(s) // saturation
+	self.l = new Prop(l) // lightness
+	self.a = new Prop(a) // alpha
+	
+	self.g = new Prop(g) // gauss
+	self.r = new Prop(r) // radius
+	
+	self.obj = circleFor(self)
+	return self
+}
+
+function PSystem() {
+	var self = this
+	// goal means and variances
+	var gMean = {h: 140, s: 180, a: 200, r: 50, g: 5}
+	var gVar = {h: 10, s: 30, a: 30, r: 200, g: 2}
+	var pls = [] // particle list
+	var log = {}
+	
+	self.init = function() {
+		pls.push(new Particle(650, 650, 60, 50, 133, 197, 0.6, 5))
+		pls.push(new Particle(500, 650, 60, 100, 133, 197, 0.6, 5))
+		pls.push(new Particle(650, 500, 60, 150, 133, 197, 0.6, 5))
+		pls.push(new Particle(500, 500, 60, 180, 133, 197, 0.6, 5))
+		
+		log.items = [["h", "_"], ["h", "v"], ["h", "acc"]]
+		
+		for (var i=0; i<log.items.length; i++) {
+			var e = log.items[i]
+			var en = e[0]+" "+e[1]
+			
+			log["mean_"+en] = []
+			log["var_"+en] = []
+
+			log["chart_mean_"+en] = createChart(
+				"mean "+en, e[1] === "_" ? gMean[en[0]] : undefined)
+			log["chart_var_"+en] = createChart(
+				"var "+en, e[1] === "_" ? gVar[en[0]] : undefined)
+		}
+		
+		self.start()
+	}
+	
+	log.updateLog = function() {
+		for (var i=0; i<log.items.length; i++) {
+			var e = log.items[i]
+			var en = e[0]+" "+e[1]
+			
+			var mean_ = log["mean_"+en]
+			var var_ = log["var_"+en]
+			
+			mean_.push(mean(plsList(e[0], e[1])))
+			var_.push(variance(plsList(e[0], e[1])))
+			
+			log["chart_mean_"+en].updateChart(mean_)
+			log["chart_var_"+en].updateChart(var_)
+		}
+	}
+	
+	self.start = function() {
+		if (stopItNow)
+			return
+		
+		var stepsTdelta = 30
+		
+		log.updateLog()
+		step("h")
+		step("g")
+		
+		for (var i=0; i<pls.length; i++) {
+			pls[i].obj.feGaussianBlur.transition()
+				.duration(stepsTdelta)
+				.ease(d3.ease("linear"))
+				.attr("stdDeviation", pls[i].g._)
+			
+			var t = pls[i].obj.transition()
+				.duration(stepsTdelta)
+				.ease(d3.ease("linear"))
+				.style("fill", hsl255ToHex(pls[i].h._, pls[i].s._, pls[i].l._))
+				
+			if (i === pls.length-1)
+				t.each("end", self.start)
+		}
+	}
+	
+	var step = function(attr) {
+		var mean_ = mean(plsList(attr))
+		var var_ = variance(plsList(attr))
+		var mean_v = mean(plsList(attr, "v"))
+		
+		if (Math.random() < 0.1) {
+			var particle = pls[Math.round(Math.random()*(pls.length-1))]
+			particle[attr].activity = (Math.random()-0.5)/30
+			particle[attr].activityRounds += Math.round(Math.random()*20)
+		}
+		
+		for (var i=0; i<pls.length; i++) {
+			var p = pls[i][attr]
+			var dMean = gMean[attr] - mean_
+			var dVar = gVar[attr] - var_
+			
+			// linear prediction
+			// steps it takes @ current v to reach g_mean_h
+			var prediction = mean_v === 0 ? 1000 : dMean/mean_v
+			// the less steps to go, the more I dampen acceleration
+			// below this number, accDelta is 0, increasing above, to *1 (max)
+			var predDampen = 20
+			// the max
+			var accDeltaAbs = 0.03
+			if (Math.abs(dMean) < 10)
+				accDeltaAbs *= Math.abs(dMean)/10
+			if (prediction >= 0 && prediction <= predDampen)
+				accDeltaAbs = 0
+			if (prediction > predDampen)
+				accDeltaAbs *= 1-predDampen/prediction
+			
+			var accDelta = (dMean < 0 ? -1 : 1) * accDeltaAbs
+			if (p.activityRounds > 0) {
+				p.activityRounds--
+				accDelta += p.activity
+			}
+			
+			// accelerate to reach goal variance
+			var d_mean = p._ - mean_
+			var cor_var_sign = (var_ < gVar[attr] && d_mean > 0)
+				|| (var_ > gVar[attr] && d_mean < 0) ? 1 : -1
+			if (Math.abs(dVar) > 3
+				&& ((cor_var_sign > 0 && mean_v <= 0)
+				|| (cor_var_sign < 0 && mean_v >= 0)))
+				accDelta += cor_var_sign * 0.01
+			
+			p.acc += accDelta
+			
+			// always dampen accelation & speed
+			p.acc *= 0.90
+			p.v += p.acc
+			p.v *= 0.90
+			p._ += p.v
+			p._ = Math.max(0, p._)
+		}
+	}
+	
+	
+	var plsList = function(property, attribute) {
+		var list = []
+		if (attribute === undefined)
+			attribute = "_"
+		for (var i=0; i<pls.length; i++)
+			list.push(pls[i][property][attribute])
+		return list
+	}
+	
+	var variance = function(list) {
+		var m = mean(list)
+		var z = []
+		for (var i=0; i<list.length; i++)
+			z.push(Math.abs(list[i] - m)) // instead of a²
+		return mean(z)
+	}
+	
+	var mean = function(list) {
+		return sum(list)/list.length
+	}
+	
+	var sum = function(list) {
+		var sum = 0
+		for (var i=0; i<list.length; i++)
+			sum += list[i]
+		return sum
+	}
+	
+	self.init()
 }
 
 
@@ -90,9 +280,6 @@ function rainbow(d, i, a) {
 
 
 
-
-
-}
 
 
 
@@ -163,9 +350,23 @@ function createChart(name, orientationBaseline) {
 	return self
 }
 
+function circleFor(p) { // particle
+	return circleSimple(p.x._, p.y._, p.r._, p.h._, p.s._, p.l._, p.a._, p.g._)
+}
 
-function circleSimple(svg, x, y, r, h, s, l, a) {
-	var c = svg.append("circle")
+function circleSimple(x, y, r, h, s, l, a, g) {
+	var filterName = Math.random().toString(36).substring(7)
+	var feGaussianBlur = d3.select("#svg defs")
+		.append("filter")
+		.attr("id", filterName)
+		.attr("x", -3)
+		.attr("y", -3)
+		.attr("width", 8)
+		.attr("height", 8)
+		.append("feGaussianBlur")
+		.attr("stdDeviation", g)
+	
+	var c = d3.select("#svg").append("circle")
 		.attr("cx", 0)
 		.attr("cy", 0)
 		.attr("r", r)
@@ -177,200 +378,11 @@ function circleSimple(svg, x, y, r, h, s, l, a) {
 //			"stroke": hsl255ToHex(140, 133, 228),
 //			"stroke-opacity": 1,
 //			"stroke-width": 1.5,
-			"filter": "url(#filter3763)"
+			"filter": "url(#"+filterName+")"
 		})
+	c.feGaussianBlur = feGaussianBlur
 	return c
 }
-
-
-
-
-function Particle(svg,x,y,r,h,l,s,a,g) {
-	var self = this
-	
-	self.x = x
-	self.y = y
-	
-	// hue lightness saturation alpha
-	self.h = h
-	self.l = l
-	self.s = s
-	self.a = a
-	
-	self.g = g // gauss
-	self.r = r // radius
-	
-	self.v = 0
-	self.acc = 0
-	self.activity = 0
-	self.activityRounds = 0
-	
-	self.obj = circleSimple(svg,x,y,r,h,s,l,a)
-	
-}
-
-function PSystem() {
-	var self = this
-	
-	// goal averages/means
-	var g_mean_numberOfParticles = 10
-	var g_mean_h = 140 // 124 - 183 sind so schmerzgrenzen
-	var g_mean_s = 180
-	// the higher layers (z-index) are always brigther (!)
-	// feBlend screen may be an alternative
-	var base_l = 90
-	var g_mean_a = 200
-	
-	// rather smaller for upper layers
-	// could also scale with transform, but this also has downsides
-	var g_mean_r = 50
-	// rather higher for higher radius
-	var g_mean_g = 30
-	
-	// goal variance
-	var g_var_numberOfParticles
-	var g_var_h = 10
-	var g_var_s = 30
-//	var g_var_l 
-	var g_var_a = 30
-	
-	var g_var_r = 200
-	var g_var_g = 30
-	
-	var pls = [] // particle list
-	
-	var log = {}
-	log.items = ["h", "v", "acc"]
-	
-	for (var i=0; i<log.items.length; i++) {
-		log["mean_"+log.items[i]] = []
-		log["var_"+log.items[i]] = []
-		
-		log["chart_mean_"+log.items[i]] = createChart(
-			"mean "+log.items[i], log.items[i] === "h" ? g_mean_h : undefined) // eval("g_mean_"+log.items[i])
-		log["chart_var_"+log.items[i]] = createChart(
-			"var "+log.items[i], log.items[i] === "h" ? g_var_h : undefined)
-	}
-	
-	log.updateLog = function() {
-		for (var i=0; i<log.items.length; i++) {
-			var mean_ = log["mean_"+log.items[i]]
-			var var_ = log["var_"+log.items[i]]
-			
-			mean_.push(self.mean(pls, log.items[i]))
-			var_.push(self.variance(pls, log.items[i]))
-			
-			log["chart_mean_"+log.items[i]].updateChart(mean_)
-			log["chart_var_"+log.items[i]].updateChart(var_)
-		}
-	}
-	
-	self.start = function() {
-		if (stopItNow)
-			return
-		
-		log.updateLog()
-		var mean_h = self.mean(pls, "h")
-		var var_h = self.variance(pls, "h")
-		var mean_v = self.mean(pls, "v")
-		
-		if (Math.random() < 0.1) {
-			var particle = pls[Math.round(Math.random()*(pls.length-1))]
-			particle.activity = (Math.random()-0.5)/30
-			particle.activityRounds += Math.round(Math.random()*20)
-		}
-		
-		for (var i=0; i<pls.length; i++) {
-			var d_all_mean_h = g_mean_h - mean_h
-			var d_all_var_h = g_var_h - var_h
-			
-			
-			// linear prediction
-			// steps it takes @ current v to reach g_mean_h
-			var prediction = mean_v === 0 ? 1000 : d_all_mean_h/mean_v
-			// the less steps to go, the more I dampen acceleration
-			// below this number, accDelta is 0, increasing above, to *1 (max)
-			var predDampen = 20
-			// the max
-			var accDeltaAbs = 0.03
-			if (Math.abs(d_all_mean_h) < 10)
-				accDeltaAbs *= Math.abs(d_all_mean_h)/10
-			if (prediction >= 0 && prediction <= predDampen)
-				accDeltaAbs = 0
-			if (prediction > predDampen)
-				accDeltaAbs *= 1-predDampen/prediction
-			
-			var accDelta = (d_all_mean_h < 0 ? -1 : 1) * accDeltaAbs
-			if (pls[i].activityRounds > 0) {
-				pls[i].activityRounds--
-				accDelta += pls[i].activity
-			}
-			
-			// accelerate to reach goal variance
-			var d_mean = pls[i].h - mean_h
-			var cor_var_sign = (var_h < g_var_h && d_mean > 0)
-				|| (var_h > g_var_h && d_mean < 0) ? 1 : -1
-			if (Math.abs(d_all_var_h) > 3
-				&& ((cor_var_sign > 0 && mean_v <= 0)
-				|| (cor_var_sign < 0 && mean_v >= 0)))
-				accDelta += cor_var_sign * 0.01
-			
-			pls[i].acc += accDelta
-			
-			// always dampen accelation & speed
-			pls[i].acc *= 0.90
-			pls[i].v += pls[i].acc
-			pls[i].v *= 0.90
-			pls[i].h += pls[i].v
-			
-			pls[i].obj.transition()
-				.duration(30)
-				.ease(d3.ease("linear"))
-				.style("fill", hsl255ToHex(pls[i].h, pls[i].s, pls[i].l))
-				.each("end", i === pls.length -1 ? self.start : function() {})
-		}
-		
-	}
-	
-	self.addParticle = function(particle) {
-		pls.push(particle)
-	}
-	
-	self.variance = function(list, property) {
-		var mean = self.mean(list, property)
-		var newVals = []
-		for (var i=0; i<list.length; i++) {
-			var a = (property !== undefined ? list[i][property] : list[i]) - mean
-			newVals.push(Math.abs(a)) // instead of a²
-		}
-		return self.mean(newVals)
-	}
-	
-	self.mean = function(list, property) {
-		return self.sum(list, property)/list.length
-	}
-	
-	self.sum = function(list, property) {
-		var sum = 0
-		for (var i=0; i<list.length; i++) {
-			sum += property !== undefined ? list[i][property] : list[i]
-		}
-		return sum
-	}
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 function saveSVGshortcut() {
 	function openSVG() {
